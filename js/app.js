98394e842a76338c0ab0aa53cfda62934402aacd
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-19 15:41:35 +0200

    delete console.log()

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 // chiedi all’utente il suo nome
 const userName = prompt('Qual\'è il tuo nome?');
-console.log(userName);
 
 // chiedi all’utente il suo cognome
 const userSurname = prompt('Qual\'è il tuo cognome?');
-console.log(userSurname);
 
 // chiedi all’utente il suo colore preferito
 const userColor = prompt('Qual\'è il tuo colore preferito?');
-console.log(userColor);
 
 // scrivi sulla pagina nomecognomecolorepreferito21
 const title = document.getElementById('title');
